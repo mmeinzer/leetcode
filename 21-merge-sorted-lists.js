f6fcd50d9c2e42551608c645bb668d65661b942a
Matt Meinzer <meinzer.matthew@gmail.com>
2022-03-13 13:52:17 -0500

    Cleanup 21

--- 21-merge-sorted-lists.js
+++ 21-merge-sorted-lists.js
@@ -11,43 +11,14 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-  if (list1 === null) {
-    return list2;
-  } else if (list2 === null) {
-    return list1;
-  }
-
   let current1 = list1;
   let current2 = list2;
-  let newHead;
-  let pointer;
 
-  while (current1 !== null && current2 !== null) {
+  let newHead = new ListNode();
+  let pointer = newHead;
+
+  while (current1 && current2) {
     if (current1.val <= current2.val) {
-      if (newHead === undefined) {
-        newHead = current1;
-        pointer = current1;
-      } else {
-        pointer.next = current1;
-        pointer = pointer.next;
-      }
-
-      current1 = current1.next;
-    } else {
-      if (newHead === undefined) {
-        newHead = current2;
-        pointer = current2;
-      } else {
-        pointer.next = current2;
-        pointer = pointer.next;
-      }
-
-      current2 = current2.next;
-    }
-  }
-
-  while (current1 !== null || current2 !== null) {
-    if (current1 !== null) {
       pointer.next = current1;
       current1 = current1.next;
     } else {
@@ -58,5 +29,7 @@ var mergeTwoLists = function (list1, list2) {
     pointer = pointer.next;
   }
 
-  return newHead;
+  pointer.next = current1 || current2;
+
+  return newHead.next;
 };
